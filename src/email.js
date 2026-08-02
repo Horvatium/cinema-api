754207a1176648bcb54aa4a6174782890e66c224
Horvatium <ivan_horvat55@yahoo.com>
2026-08-02 16:01:45 +0200

    Update email.js v2

--- src/email.js
+++ src/email.js
@@ -2,17 +2,17 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com', 
-    port: 587, 
+    host: 'smtp.gmail.com',
+    port: 587,
     secure: false,
     family: 4,
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
-    tls: {
-        rejectUnauthorized: false
-    }
 });
 
 // PREDLOGE E-POŠTNIH SPOROČIL
