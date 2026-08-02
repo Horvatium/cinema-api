9f3f37589dc495b26436b4a2589c0230ae822da6
Vid <horvatium@gmail.com>
2026-08-02 14:43:50 +0200

    poprava email

--- src/email.js
+++ src/email.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
+    family: 4,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
